3fc7a2df9e8338930209b28d58c6739d4abaf900
Jihan Jasper Al Rashid <johndoe@example.com>
2025-01-30 02:18:01 +0600

    fix(frontend): update "next-intl" to work with Next.js 15

--- apps/frontend/src/i18n/request.ts
+++ apps/frontend/src/i18n/request.ts
@@ -2,10 +2,12 @@ import { routing } from "./routing";
 import { getRequestConfig } from "next-intl/server";
 import { notFound } from "next/navigation";
 
-export default getRequestConfig(async ({ locale }) => {
+export default getRequestConfig(async ({ requestLocale }) => {
+  let locale = await requestLocale;
   if (!routing.locales.includes(locale as "en" | "de")) notFound();
 
   return {
+    locale,
     messages: (await import(`../../messages/${locale}.json`)).default,
   };
 });
